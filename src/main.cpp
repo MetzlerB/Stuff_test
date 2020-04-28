dcd615b659012db30d931250cfa9c4459f184027
Alex UnHold <alex@hold.re>
2020-04-28 18:46:51 +0200

    added button press led on code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+  pinMode(6, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  if(digitalRead(6)){
+    digitalWrite(3, HIGH);
+  }else{
+    digitalWrite(3, LOW);
+  }
 }
